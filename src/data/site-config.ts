722ac1cf05b2947c7ae23aef22799a653a89b9f4
Nick Marks <nickmarks00@gmail.com>
2025-06-19 21:11:42 +1000

    feat: Create foundational site config
    This defines the main types and structure of the site, including header
    and navigation links.

---
+++ src/data/site-config.ts
@@ -0,0 +1,119 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    website: string;
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    website: 'https://chumbers.io',
+    title: 'Chumbers',
+    subtitle: 'A blog site',
+    description: 'A repository for my thoughts, reflections and learnings',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme',
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/',
+        },
+        {
+            text: 'Projects',
+            href: '/projects',
+        },
+        {
+            text: 'Blog',
+            href: '/blog',
+        },
+        {
+            text: 'Categories',
+            href: '/categories',
+        },
+        {
+            text: 'Tags',
+            href: '/tags',
+        },
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about',
+        },
+        {
+            text: 'Contact',
+            href: '/contact',
+        },
+    ],
+    socialLinks: [
+        {
+            text: 'Instagram',
+            href: 'https://instragram.com/__nickmx',
+        },
+        {
+            text: 'GitHub',
+            href: 'https://github.com/chumb3rs',
+        },
+        {
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/nickmarks00',
+        },
+    ],
+    hero: {
+        title: 'Hi There & Welcome to My Corner of the Web!',
+        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        image: {
+            src: '/hero.jpeg',
+            alt: 'A person sitting at a desk in front of a computer',
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact',
+            },
+        ],
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#',
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8,
+};
+
+export default siteConfig;
