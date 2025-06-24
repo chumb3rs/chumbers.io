95d707bc1d8876c90a96b065b76c983e9472eb2a
Nick Marks <nickmarks00@gmail.com>
2025-06-24 22:12:08 +1000

    feat: Install serif and sans-serif fonts and configure Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				heading: ['"EB Garamond Variable"', 'serif'],
+				body: ['"Manrope Variable"', 'sans-serif'],
+				code: ['"JetBrains Mono Variable"', 'monospace']
+			}
+		},
 	},
 	plugins: [],
 }
