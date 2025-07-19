90357d61b912848a410f34e3776cb1ac72b8fe9d
Nick Marks <nickmarks00@gmail.com>
2025-07-19 17:28:55 +1000

    feat: Update Footer with correct theming
    This is a first version attempt of styling the page footer. Changes to
    consider for future:
    - Should I replace the social links with icons? If so, create an
      `IconLink` component and a `TextLink` component that extend a generic
      `NavLink` component
    - Consider whether there are any further links or bits of information
      you want to include here
      - I think the `.font-code` decorator is invalid, this needs to be
        fixed

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,3 +1,6 @@
+import type { IconType } from 'react-icons';
+import { FaGithub, FaInstagram, FaYoutube } from 'react-icons/fa';
+
 export type Image = {
     src: string;
     alt?: string;
@@ -7,6 +10,7 @@ export type Image = {
 export type Link = {
     text: string;
     href: string;
+    icon?: IconType;
 };
 
 export type Hero = {
@@ -39,85 +43,93 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    website: 'https://chumbers.io',
+    title: 'Chumbers',
+    subtitle: 'The place I call home',
+    description:
+        'A personal essay site for Chumb3rs',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'Dante - Astro.js and Tailwind CSS theme',
     },
     headerNavLinks: [
         {
             text: 'Home',
-            href: '/'
+            href: '/',
         },
         {
             text: 'Projects',
-            href: '/projects'
+            href: '/projects',
         },
         {
             text: 'Blog',
-            href: '/blog'
+            href: '/blog',
+        },
+        {
+            text: 'Categories',
+            href: '/categories',
         },
         {
             text: 'Tags',
-            href: '/tags'
-        }
+            href: '/tags',
+        },
     ],
     footerNavLinks: [
         {
             text: 'About',
-            href: '/about'
+            href: '/about',
         },
         {
             text: 'Contact',
-            href: '/contact'
+            href: '/contact',
         },
         {
-            text: 'Terms',
-            href: '/terms'
+            text: 'Subscribe',
+            href: '/subscribe',
         },
         {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
+            text: 'Resume',
+            href: '/resume',
+        },
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
             text: 'Instagram',
-            href: 'https://instagram.com/'
+            href: 'https://instagram.com/',
+            icon: FaInstagram,
         },
         {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
-        }
+            text: 'GitHub',
+            href: 'https://github.com/chumb3rs',
+            icon: FaGithub,
+        },
+        {
+            text: 'YouTube',
+            href: 'https://www.youtube.com/@chumbers.',
+            icon: FaYoutube,
+        },
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: 'A person sitting at a desk in front of a computer',
         },
         actions: [
             {
                 text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
+                href: '/contact',
+            },
+        ],
     },
     subscribe: {
         title: 'Subscribe to Dante Newsletter',
         text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
+        formUrl: '#',
     },
     postsPerPage: 8,
-    projectsPerPage: 8
+    projectsPerPage: 8,
 };
 
 export default siteConfig;
