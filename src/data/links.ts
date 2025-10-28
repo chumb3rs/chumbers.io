8f97495cccee6fd471655cc9cec994e24708445a
Nick Marks <nickmarks00@gmail.com>
2025-10-28 21:46:24 +1100

    feat: Configure project development environment
    I have migrated all of the necessary npm packages over so that I am
    using Biome as my linter/formatter of choice. This includes run scripts
    that will format and lint automatically.

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,17 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
-import GithubIcon from '@/components/icons/GithubIcon'
+import TwitterIcon from '@/components/icons/TwitterIcon';
+import GithubIcon from '@/components/icons/GithubIcon';
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
-	{
-		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: GithubIcon
-	},
+    {
+        name: 'Github',
+        url: 'https://github.com/danielcgilibert/blog-template',
+        icon: GithubIcon,
+    },
 
-	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
-	}
-] as const
+    {
+        name: 'Twitter',
+        url: 'https://github.com/danielcgilibert/blog-template',
+        icon: TwitterIcon,
+    },
+] as const;
