bd6cdcce8ec3a3172e0bdfb15e525acbb0bff0a6
Nick Marks <nickmarks00@gmail.com>
2025-11-03 20:28:05 +1100

    chore: Correct GitHub social link and remove Twitter/X link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,18 +1,12 @@
 import type { SocialNetwork } from 'src/types/socials';
 import GithubIcon from '@/components/icons/GithubIcon';
 import InstagramIcon from '@/components/icons/InstagramIcon';
-import TwitterIcon from '@/components/icons/TwitterIcon';
 
 export const SOCIALNETWORKS: SocialNetwork[] = [
     {
         name: 'Github',
-        url: 'https://github.com/chumb3rs/chumbers.io',
+        url: 'https://github.com/chumb3rs',
         icon: GithubIcon,
-    },
-    {
-        name: 'Twitter',
-        url: 'https://github.com/danielcgilibert/blog-template',
-        icon: TwitterIcon,
     },
     {
         name: 'Instagram',
